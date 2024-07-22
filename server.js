cb5cef302047549f5a950087eaa8caa180111f93
dennismutitu <dennismutitu06@gmail.com>
2024-07-22 11:32:25 +0300

    i created a server using node js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const { createServer } = require('node:http');
-//or
-//import {createServer} from node:http;
-// createServer() creates a new HTTP server and returns it.
-//this server is set to listen to the following port and hostname
+
 const hostname = '127.0.0.1';
 const port = 3000;
-//this callback func is called when the server is ready
+
 const server = createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
+	  res.statusCode = 200;
+	  res.setHeader('Content-Type', 'text/plain');
+	  res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`
+	  console.log(`Server running at http://${hostname}:${port}/`);
 });
